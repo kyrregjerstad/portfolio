791a1abc5df87c2f03f56f6b12d92fc610be1caf
Kyrre Gjerstad <kyrregjerstad@gmail.com>
2024-05-02 22:41:01 +0200

    add border animation

--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -1,4 +1,6 @@
 <script>
+	import Border from './Border.svelte';
+
 	import '@/app.pcss';
 	const { children } = $props();
 
@@ -6,8 +8,9 @@
 
 
 <!-- we could make the border animate in, wrapping the screen -->
-<div class="border-background h-dvh border-[2.5rem]">
-	<main class="border-foreground h-full overflow-auto border-2 p-10">
+<div class="border-background h-dvh border-[2.5rem] relative">
+	<Border />
+	<main class="border-foreground h-full overflow-auto p-10">
 		{@render children()}
 	</main>
 </div>
---
+++ app/src/routes/Border.svelte
@@ -0,0 +1,22 @@
+<script>
+	import { cubicInOut } from 'svelte/easing';
+	import { draw } from 'svelte/transition';
+
+	let mounted = $state(false);
+
+	const commands = 'M0,0 L100,0 L100,100 L0,100 z';
+
+	$effect(() => {
+		mounted = true;
+	});
+</script>
+
+{#if mounted}
+	<svg
+		class="stroke-foreground pointer-events-none fixed inset-0 h-[calc(100dvh_-_5rem)] w-[calc(100vw_-_5rem)] translate-x-[2.5rem] translate-y-[2.5rem]"
+		viewBox="0 0 100 100"
+		preserveAspectRatio="none"
+	>
+		<path transition:draw={{ duration: 1800, easing: cubicInOut }} d={commands} fill="none" stroke-width="0.1px" />
+	</svg>
+{/if}
