72f1a610b67f6b6eadd7eed41dcc2d7831ccc764
Kyrre Gjerstad <kyrregjerstad@gmail.com>
2024-05-29 20:20:17 +0200

    add posthog

--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -2,6 +2,14 @@
 	import '@/app.pcss';
 	import Border from './Border.svelte';
 	import { Toaster } from 'svelte-sonner';
+	import posthog from 'posthog-js';
+	import { browser } from '$app/environment';
+	import { beforeNavigate, afterNavigate } from '$app/navigation';
+
+	if (browser) {
+		beforeNavigate(() => posthog.capture('$pageleave'));
+		afterNavigate(() => posthog.capture('$pageview'));
+	}
 
 	const { children } = $props();
 </script>
---
+++ app/src/routes/+layout.ts
@@ -0,0 +1,15 @@
+// src/routes/+layout.js
+import posthog from 'posthog-js';
+import { browser } from '$app/environment';
+import { PUBLIC_POSTHOG_API_KEY, PUBLIC_POSTHOG_HOST } from '$env/static/public';
+
+export const load = async () => {
+	if (browser) {
+		posthog.init(PUBLIC_POSTHOG_API_KEY, {
+			api_host: PUBLIC_POSTHOG_HOST,
+			capture_pageview: false,
+			capture_pageleave: false,
+		});
+	}
+	return;
+};
