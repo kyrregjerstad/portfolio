4ba6c5d9a03063cc474db5ad0b261a4663b3c3ee
Kyrre Gjerstad <kyrregjerstad@gmail.com>
2024-06-06 13:34:15 +0200

    update theme cookie

--- app/src/hooks.server.ts
+++ app/src/hooks.server.ts
@@ -1,7 +1,16 @@
 import type { Handle } from '@sveltejs/kit';
 
 export const handle = (async ({ event, resolve }) => {
-	const theme = event.cookies.get('theme') || 'dark';
+	const themeCookie = event.cookies.get('theme');
+
+	if (!themeCookie) {
+		event.cookies.set('theme', 'dark', {
+			path: '/',
+			maxAge: 60 * 60 * 24 * 365, // 1 year
+		});
+	}
+
+	const theme = themeCookie || 'dark';
 
 	return resolve(event, {
 		transformPageChunk: ({ html }) => html.replace(`data-theme=""`, `data-theme="${theme}"`),
