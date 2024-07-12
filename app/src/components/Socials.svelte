45155a00c87ddb0a5f83b8ac64a03cf42698c87d
Kyrre Gjerstad <kyrregjerstad@gmail.com>
2024-07-12 10:06:14 +0200

    update description (again)...

--- app/src/components/Socials.svelte
+++ app/src/components/Socials.svelte
@@ -1,3 +1,8 @@
+<meta property="og:site_name" content="Kyrre Gjerstad" />
+<meta property="og:url" content="https://kyrre.dev" />
+<meta property="og:type" content="website" />
+<meta property="og:title" content="Kyrre Gjerstad - Full-stack developer" />
+<meta property="og:image" content="https://kyrre.dev/images/og-kyrre-gjerstad.jpg" />
 <meta
 	property="og:description"
 	content="Portfolio of Kyrre Gjerstad, a full-stack developer specializing in NextJS and SvelteKit."
