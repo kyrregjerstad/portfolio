440b1f5af141243c7577caa7080fbff06217810c
Kyrre Gjerstad <kyrregjerstad@gmail.com>
2024-07-12 10:02:56 +0200

    add og Image

---
+++ app/src/components/Socials.svelte
@@ -0,0 +1,5 @@
+<meta property="og:site_name" content="Kyrre Gjerstad" />
+<meta property="og:url" content="https://kyrre.dev" />
+<meta property="og:type" content="website" />
+<meta property="og:title" content="Kyrre Gjerstad - Full-stack developer" />
+<meta property="og:image" content="https://kyrre.dev/images/og-kyrre-gjerstad.jpg" />
