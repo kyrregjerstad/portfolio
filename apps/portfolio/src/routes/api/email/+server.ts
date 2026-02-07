188f3e3c13ed98f699bad636f4070d09d6c1ccf7
Kyrre Gjerstad <kyrregjerstad@gmail.com>
2026-02-07 19:50:04 +0100

    update to use resend

--- apps/portfolio/src/routes/api/email/+server.ts
+++ apps/portfolio/src/routes/api/email/+server.ts
@@ -1,25 +1,24 @@
 import { error, json } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
-import { PRIVATE_SENDGRID_API_KEY } from '$env/static/private';
-import sgMail from '@sendgrid/mail';
+import { RESEND_API_KEY } from '$env/static/private';
+import { Resend } from 'resend';
 import { contactSchema } from '$lib/schema/contactSchema';
 
-sgMail.setApiKey(PRIVATE_SENDGRID_API_KEY);
+const resend = new Resend(RESEND_API_KEY);
 
 export const POST: RequestHandler = async ({ request }) => {
 	try {
 		const body = await request.json();
 		const { name, email, subject, message } = contactSchema.parse(body);
 
-		const msg = {
+		await resend.emails.send({
 			to: 'kyrregjerstad@gmail.com',
 			from: 'hi@kyrre.dev',
 			subject: subject,
 			text: message,
 			html: `<p>new message from ${name}, email: ${email}</p> <p>${message}</p>`,
-		};
+		});
 
-		await sgMail.send(msg);
 		return json({ success: true });
 	} catch (e) {
 		console.log(e);
